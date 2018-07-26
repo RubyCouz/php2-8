6409d92da236c3375ace67081c74d6c44c486b90
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-26 08:50:24 +0200

    exo8

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$isOk = true; // définition de la variable
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice 8</title>
+   </head>
+   <body>
+     <p>
+<?php
+if ($isOk)
+{
+  echo 'c\'est bon';
+} else {
+  echo 'c\'est pas bon';
+}
+ ?>
+     </p>
+   </body>
+ </html>
